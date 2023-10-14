4e8bacc7d09f0ec583e536833193c9999d1ff438
DESKTOP-FC6EDPL\User <truongthetan1601@gmail.com>
2023-10-14 23:28:07 +0700

    add API-MarkMemberQuit and API-UpdateMember

---
+++ SELMs/Services/IMemberService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace SELMs.Services
+{
+	public interface IMemberService
+	{
+		Task<dynamic> MarkMemberQuit(dynamic member);
+
+		Task<dynamic> UpdateMember(dynamic member);
+		//Task<dynamic> GetMemberByUsername(string username);
+	}
+}
