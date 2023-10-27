487a4f246121fc4cff10c30bc7a6d6f477c9f15f
DatLT <datlthe151200@fpt.edu.vn>
2023-10-27 21:09:45 +0700

    Update DTO Mappings

--- SELMs/Api/DTOs/UserDTO.cs
+++ SELMs/Api/DTOs/UserDTO.cs
@@ -14,14 +14,17 @@ namespace SELMs.Api.DTOs
         public string fullname { get; set; }
         public string role_code { get; set; }
         public Nullable<bool> is_admin { get; set; }
-        public Nullable<System.DateTime> date_of_birth { get; set; }
+        //public Nullable<System.DateTime> date_of_birth { get; set; }
+        public string date_of_birth { get; set; }
         public string hotline { get; set; }
         public string email { get; set; }
         public Nullable<bool> gender { get; set; }
         public string address { get; set; }
         public Nullable<int> avatar_img { get; set; }
-        public Nullable<System.DateTime> hire_date { get; set; }
-        public Nullable<System.DateTime> resignation_date { get; set; }
+        //public Nullable<System.DateTime> hire_date { get; set; }
+        public string hire_date { get; set; }
+        //public Nullable<System.DateTime> resignation_date { get; set; }
+        public string resignation_date { get; set; }
         public string work_term { get; set; }
         public string skill { get; set; }
         public string job_description { get; set; }
--- SELMs/App_Start/MapperConfig.cs
+++ SELMs/App_Start/MapperConfig.cs
@@ -16,7 +16,15 @@ namespace SELMs.App_Start
             var config = new MapperConfiguration(cfg =>
             {
                 //Configuring Employee and EmployeeDTO
-                cfg.CreateMap<UserDTO, User>().ReverseMap();
+                cfg.CreateMap<UserDTO, User>()
+                .ForMember(des => des.date_of_birth, act => act.MapFrom(src => DateTime.Parse(src.date_of_birth)))
+                .ForMember(des => des.hire_date, act => act.MapFrom(src => DateTime.Parse(src.hire_date)))
+                .ForMember(des => des.resignation_date, act => act.MapFrom(src => DateTime.Parse(src.resignation_date)));
+
+                cfg.CreateMap<User, UserDTO>()
+                .ForMember(des => des.date_of_birth, act => act.MapFrom(src => src.date_of_birth.ToString()))
+                .ForMember(des => des.hire_date, act => act.MapFrom(src => src.hire_date.ToString()))
+                .ForMember(des => des.resignation_date, act => act.MapFrom(src => src.resignation_date.ToString()));
 
                 //Configuring Category and CategoryDTO
                 cfg.CreateMap<CategoryDTO, Category>().ReverseMap();
