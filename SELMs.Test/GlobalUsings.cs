a8c9affd4a9f912c51c25d9d2181b64da8b4d0b3
DESKTOP-FC6EDPL\User <truongthetan1601@gmail.com>
2023-11-04 18:58:32 +0700

    add global using

--- SELMs.Test/GlobalUsings.cs
+++ SELMs.Test/GlobalUsings.cs
@@ -1,1 +1,20 @@
 global using Xunit;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Hosting;
+using System.Web.Http.Routing;
+
+public static class ApiControllerSetup
+{
+	public static void SetupController(ApiController apiController)
+	{
+		var routeData = new HttpRouteData(new HttpRoute());
+		apiController.Configuration = new HttpConfiguration();
+		apiController.Request = new HttpRequestMessage();
+		apiController.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = apiController.Configuration;
+		apiController.ControllerContext = new HttpControllerContext(apiController.Configuration, routeData, apiController.Request)
+		{
+			Controller = apiController
+		};
+	}
+}
