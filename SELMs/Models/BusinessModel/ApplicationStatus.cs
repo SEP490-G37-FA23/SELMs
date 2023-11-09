7927af477d2819a933ca54ea3edd5eb5b5d0fc4e
DatLT <datlthe151200@fpt.edu.vn>
2023-11-09 21:43:45 +0700

    Resolve project configs

---
+++ SELMs/Models/BusinessModel/ApplicationStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SELMs.Models.BusinessModel
+{
+    public static class ApplicationStatus
+    {
+        public const string DRAFT = "DRAFT";
+        public const string PENDING = "PENDING";
+        public const string APPROVED = "APPROVED";
+        public const string REJECTED = "REJECTED";
+        public const string CANCELED = "CANCELED";
+    }
+}
