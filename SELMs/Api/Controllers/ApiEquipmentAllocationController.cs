72efd2b0277e3f864b0fb5357cd3e86d2ae981cf
MaiThiLy <lymthe153498@fpt.edu.vn>
2023-11-14 00:41:08 +0700

    lymt_14112023_l1
    update equipment_allowcation

--- SELMs/Api/Controllers/ApiEquipmentAllocationController.cs
+++ SELMs/Api/Controllers/ApiEquipmentAllocationController.cs
@@ -28,6 +28,27 @@ namespace SELMs.Api.Controllers
         private IMapper mapper = MapperConfig.Initialize();
 
         #region Get application by id
+        [HttpPost]
+        [Route("equipment-allocation-application/search")]
+        public async Task<IHttpActionResult> GetEAAList(Argument arg)
+        {
+            try
+            {
+                dynamic returnedData = null;
+                returnedData = repository.GetEAAList(arg);
+                return Ok(returnedData);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error: ", ex);
+                Console.WriteLine($"{ex.Message} \n {ex.StackTrace}");
+                return BadRequest($"{ex.Message} \n {ex.StackTrace}");
+                throw;
+            }
+        }
+        #endregion
+
+        #region Get application by id
         [HttpGet]
         [Route("equipment-allocation-application/{id}")]
         public async Task<IHttpActionResult> GetApplication(int id)
