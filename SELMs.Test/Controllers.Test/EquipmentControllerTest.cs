d196699b0ad2be41f4b4c92b00b91aff1aaf788a
DESKTOP-FC6EDPL\User <truongthetan1601@gmail.com>
2023-11-21 15:31:08 +0700

    update unit for equiment feature

--- SELMs.Test/Controllers.Test/EquipmentControllerTest.cs
+++ SELMs.Test/Controllers.Test/EquipmentControllerTest.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Http.Results;
-using Newtonsoft.Json;
 
 namespace SELMs.Test.Controllers.Test
 {
@@ -76,6 +75,74 @@ namespace SELMs.Test.Controllers.Test
 				Assert.Fail("Test case failed\n" + ex.Message);
 			}
 		}
+
+
+
+
+		[Theory]
+		[MemberData(nameof(EquipmentControllerTestData.CreateEquipmentTestData), MemberType = typeof(EquipmentControllerTestData))]
+		public async Task TestSaveEquipmentBySystemCode_ReturnStatusCode200(EquipmentNew equipmentNew)
+		{
+			try
+			{
+				var actionResult = await apiEquipmentController.SaveEquipment(equipmentNew);
+				var response = await actionResult.ExecuteAsync(CancellationToken.None);
+
+				Assert.Equal(200, (int)response.StatusCode);
+				output.WriteLine($"Status code: {(int)response.StatusCode}");
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail("Test case failed\n" + ex.Message);
+			}
+		}
+
+
+
+
+
+
+		[Theory]
+		[MemberData(nameof(EquipmentControllerTestData.ImportEquipmentTestData), MemberType = typeof(EquipmentControllerTestData))]
+		public async Task TestImportEquipmentBySystemCode_ReturnStatusCode200(List<EquipmentDTO> equipmentDTOs)
+		{
+			try
+			{
+				var actionResult = await apiEquipmentController.ImportEquipments(equipmentDTOs);
+				var response = await actionResult.ExecuteAsync(CancellationToken.None);
+				Thread.Sleep(3000);
+
+				Assert.Equal(200, (int)response.StatusCode);
+				output.WriteLine($"Status code: {(int)response.StatusCode}");
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail("Test case failed\n" + ex.Message);
+			}
+		}
+
+
+
+
+		[Theory]
+		[MemberData(nameof(EquipmentControllerTestData.UpdateEquipmentTestData), MemberType = typeof(EquipmentControllerTestData))]
+		public async Task TestUpdateEquipmentBySystemCode_ReturnStatusCode200(int id, EquipmentDTO equipment)
+		{
+			try
+			{
+				var actionResult = await apiEquipmentController.UpdateEquipment(id, equipment);
+
+				Thread.Sleep(3000);
+				var response = await actionResult.ExecuteAsync(CancellationToken.None);
+
+				Assert.Equal(200, (int)response.StatusCode);
+				output.WriteLine($"Status code: {(int)response.StatusCode}");
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail("Test case failed\n" + ex.Message);
+			}
+		}
 		#endregion
 	}
 
@@ -84,6 +151,67 @@ namespace SELMs.Test.Controllers.Test
 
 	public static class EquipmentControllerTestData
 	{
+		public static IEnumerable<object[]> CreateEquipmentTestData()
+		{
+			EquipmentDTO e1 = new() { system_equipment_code = "E0123", equipment_name = "hoho" };
+			EquipmentDTO e2 = new() { system_equipment_code = "E0456", equipment_name = "haha" };
+			EquipmentDTO e3 = new() { system_equipment_code = "E0789", equipment_name = "hihi" };
 
+
+			List<EquipComponentDTO> list1 = new()
+			{
+				new EquipComponentDTO() { equipment_name = "new new", standard_equipment_code = "E0101", system_equipment_code = "E1111" },
+				new EquipComponentDTO() { equipment_name = "old", standard_equipment_code = "E0102", system_equipment_code = "E1112" }
+			};
+
+			List<EquipComponentDTO> list2 = new()
+			{
+				new EquipComponentDTO() { equipment_name = "very old", standard_equipment_code = "E0103", system_equipment_code = "E1113" },
+				new EquipComponentDTO() { equipment_name = "null", standard_equipment_code = "E0104", system_equipment_code = "E1114" }
+			};
+
+			List<EquipComponentDTO> list3 = new()
+			{
+				new EquipComponentDTO() { equipment_name = "asd", standard_equipment_code = "E0105", system_equipment_code = "E1115" },
+				new EquipComponentDTO() { equipment_name = "hí hí", standard_equipment_code = "E0106", system_equipment_code = "E1116" }
+			};
+
+
+			yield return new object[] { new EquipmentNew() { equip = e1, ListComponentEquips = list1, location_id = 1, img = null } };
+			yield return new object[] { new EquipmentNew() { equip = e2, ListComponentEquips = list2, location_id = 1, img = null } };
+			yield return new object[] { new EquipmentNew() { equip = e3, ListComponentEquips = list3, location_id = 1, img = null } };
+		}
+
+
+
+
+		public static IEnumerable<object[]> ImportEquipmentTestData()
+		{
+			List<EquipmentDTO> list1 = new()
+			{
+
+				new EquipmentDTO(){equipment_name="GSG9"},
+				new EquipmentDTO(){equipment_name="GIGN"}
+			};
+
+			List<EquipmentDTO> list2 = new()
+			{
+
+				new EquipmentDTO(){ equipment_name="SAS"},
+				new EquipmentDTO(){ equipment_name="SEAL"}
+			};
+
+			yield return new object[] { list1 };
+			yield return new object[] { list2 };
+		}
+
+
+
+		public static IEnumerable<object[]> UpdateEquipmentTestData()
+		{
+			yield return new object[] { 0, new EquipmentDTO() { system_equipment_code = "E0010", equipment_name = "no non" } };
+			yield return new object[] { 21, new EquipmentDTO() { system_equipment_code = "E0021", equipment_name = "hàng xịn" } };
+			yield return new object[] { 22, new EquipmentDTO() { system_equipment_code = "E0022", equipment_name = "null null" } };
+		}
 	}
 }
--- SELMs/Services/Implements/EquipmentService.cs
+++ SELMs/Services/Implements/EquipmentService.cs
@@ -1,57 +1,54 @@
-﻿using SELMs.Api.DTOs;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SELMs.Api.DTOs;
 using SELMs.Models;
 using SELMs.Repositories;
 using SELMs.Repositories.Implements;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
 
 namespace SELMs.Services.Implements
 {
-    public class EquipmentService : IEquipmentService
-    {
-        private IEquipmentRepository repository = new EquipmentRepository();
-        public async Task ImportEquipments(List<Equipment> equipments)
-        {
-            Equipment obj = repository.GetLastEquipment();
-            int num = obj == null ? 1 : obj.equipment_id;
-            foreach (Equipment equip in equipments)
-            {
-                string code = "E";
-                code += num < 10000 ? num.ToString("D4") : num.ToString();
-                equip.system_equipment_code = code;
-            }
-            repository.SaveEquipments(equipments);
-        }
+	public class EquipmentService : IEquipmentService
+	{
+		private IEquipmentRepository repository = new EquipmentRepository();
+		public async Task ImportEquipments(List<Equipment> equipments)
+		{
+			Equipment obj = repository.GetLastEquipment();
+			int num = obj == null ? 1 : obj.equipment_id;
+			foreach (Equipment equip in equipments)
+			{
+				string code = "E";
+				code += num < 10000 ? num.ToString("D4") : num.ToString();
+				equip.system_equipment_code = code;
+			}
+			repository.SaveEquipments(equipments);
+		}
 
 
-        public async Task SaveEquipment(Equipment equipment, int location_id, List<EquipComponentDTO> ListComponentEquips)
-        {
-            Equipment eq = equipment;
-            string code = GenerateEquipmentCode();
-            eq.system_equipment_code = code;
-            repository.SaveEquipment(eq, location_id, ListComponentEquips);
+		public async Task SaveEquipment(Equipment equipment, int location_id, List<EquipComponentDTO> ListComponentEquips)
+		{
+			Equipment eq = equipment;
+			string code = GenerateEquipmentCode();
+			eq.system_equipment_code = code;
+			repository.SaveEquipment(eq, location_id, ListComponentEquips);
 
-        }
+		}
 
-        public async Task UpdateEquipment(int id, Equipment equipment)
-        {
-            if (await repository.GetEquipment(id) != null)
-            {
-                equipment.equipment_id = id;
-                repository.UpdateEquipment(equipment);
-            }
-        }
+		public async Task UpdateEquipment(int id, Equipment equipment)
+		{
+			if (repository.GetEquipment(id) != null)
+			{
+				equipment.equipment_id = id;
+				repository.UpdateEquipment(equipment);
+			}
+		}
 
-        string GenerateEquipmentCode()
-        {
-            string eqmtCode = "E";
-            dynamic equipment = repository.GetLastEquipment();
-            int num = equipment == null ? 1 : equipment.equipment_id + 1;
-            eqmtCode += num < 10000 ? num.ToString("D4") : num.ToString();
-            return eqmtCode;
-        }
-    }
+		string GenerateEquipmentCode()
+		{
+			string eqmtCode = "E";
+			dynamic equipment = repository.GetLastEquipment();
+			int num = equipment == null ? 1 : equipment.equipment_id + 1;
+			eqmtCode += num < 10000 ? num.ToString("D4") : num.ToString();
+			return eqmtCode;
+		}
+	}
 }
